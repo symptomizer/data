f7eccbf71cb803bc38044c1735a2831be023fb03
Greg Brimble <developer@gregbrimble.com>
2021-01-26 19:02:33 +0000

    Add MedicinesComplete

--- src/nhs.ts
+++ src/nhs.ts
@@ -1,3 +1,4 @@
+import { ONE_WEEK } from "./constants";
 import { addHeaders, alterURL } from "@glenstack/cf-workers-fetch-helpers";
 
 const nhsFetch = alterURL(
@@ -15,11 +16,6 @@ const nhsFetch = alterURL(
     },
   }
 );
-
-const ONE_MINUTE = 60;
-const ONE_HOUR = ONE_MINUTE * 60;
-const ONE_DAY = ONE_HOUR * 24;
-const ONE_WEEK = ONE_DAY * 7;
 
 export const handleRequest = async (request: Request): Promise<Response> =>
   nhsFetch(request, {
