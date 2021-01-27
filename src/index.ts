dabee761f74ee81888d44e1d48b541d1ab796993
Greg Brimble <developer@gregbrimble.com>
2021-01-27 11:10:34 +0000

    Revert Evidence API

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
 import { Router } from "@glenstack/cf-workers-router";
 import { handleRequest as nhs } from "./nhs";
 import { handleRequest as medicinesComplete } from "./medicinesComplete";
-import {
-  handleStatsRequest as evidenceStats,
-  handleTriggerRequest as evidenceTrigger,
-  handleDocumentRequest as evidenceDocument,
-  handleSchedule as evidenceSchedule,
-} from "./evidence";
 
 const router = new Router();
 router.get("/nhs/.*", nhs);
 router.get("/medicinesComplete/.*", medicinesComplete);
-router.get("/evidence/stats", evidenceStats);
-router.get("/evidence/trigger", evidenceTrigger);
-router.get("/evidence/.*", evidenceDocument);
 
 addEventListener("fetch", (event) => {
   event.respondWith(router.route(event.request));
-});
-
-addEventListener("scheduled", (event) => {
-  event.waitUntil(
-    (async () => {
-      await evidenceSchedule();
-    })()
-  );
 });
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,2 @@
 declare const NHS_API_KEY: string;
 declare const MEDICINESCOMPLETE_PLAY_SESSION_JWT: string;
-declare const EVIDENCE: KVNamespace;
