0920c4119793b0568eeee9af689a8a81a0f38b4a
Greg Brimble <developer@gregbrimble.com>
2021-01-27 02:19:44 +0000

    Evidence API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import { Router } from "@glenstack/cf-workers-router";
 import { handleRequest as nhs } from "./nhs";
 import { handleRequest as medicinesComplete } from "./medicinesComplete";
+import {
+  handleRequest as evidence,
+  handleDocumentRequest as evidenceDocument,
+  handleSchedule as evidenceSchedule,
+} from "./evidence";
 
 const router = new Router();
 router.get("/nhs/.*", nhs);
 router.get("/medicinesComplete/.*", medicinesComplete);
+router.get("/evidence/", evidence);
+router.get("/evidence/.*", evidenceDocument);
 
 addEventListener("fetch", (event) => {
   event.respondWith(router.route(event.request));
+});
+
+addEventListener("scheduled", (event) => {
+  event.waitUntil(
+    (async () => {
+      await evidenceSchedule();
+    })()
+  );
 });
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,3 @@
 declare const NHS_API_KEY: string;
 declare const MEDICINESCOMPLETE_PLAY_SESSION_JWT: string;
+declare const EVIDENCE: KVNamespace;
