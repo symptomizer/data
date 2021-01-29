4aab6a99e0ba3157b08880b10c6b18073d3b3ee5
Greg Brimble <developer@gregbrimble.com>
2021-01-29 17:46:52 +0000

    Simplify WHO document fetcher

--- src/who.ts
+++ src/who.ts
@@ -1,34 +1,28 @@
 import { ONE_WEEK } from "./constants";
-import { alterURL } from "@glenstack/cf-workers-fetch-helpers";
 
-const FILENAME_REGEX = /<meta content="(.*)" name="citation_pdf_url">/gm;
-
-const whoFetch = alterURL(fetch, {
-  mutate: (url) => {
-    const whoURL = new URL(url);
-    whoURL.hostname = "apps.who.int";
-    whoURL.pathname = whoURL.pathname.split("/who")[1];
-    whoURL.pathname = `/iris/handle${whoURL.pathname}`;
-    return whoURL.toString();
-  },
-});
+const PDF_URL_REGEX = /<meta content="(.*)" name="citation_pdf_url">/gm;
 
 export const handleRequest = async (request: Request): Promise<Response> => {
-  const response = await whoFetch(request, {
+  const documentPageURL = new URL(request.url);
+  documentPageURL.hostname = "apps.who.int";
+  documentPageURL.pathname = documentPageURL.pathname.split("/who")[1];
+  documentPageURL.pathname = `/iris/handle${documentPageURL.pathname}`;
+
+  const documentPage = await fetch(documentPageURL.toString(), {
     cf: {
       cacheEverything: true,
       cacheTtl: ONE_WEEK,
     },
   });
 
-  const text = await response.text();
-  const match = FILENAME_REGEX.exec(text);
+  const documentPageHTML = await documentPage.text();
+  const match = PDF_URL_REGEX.exec(documentPageHTML);
 
   if (match === null) {
     return new Response(null, { status: 404 });
   } else {
-    const pdfURL = match[1];
-    return fetch(pdfURL, {
+    const documentPDFURL = match[1];
+    return fetch(documentPDFURL, {
       cf: { cacheEverything: true, cacheTtl: ONE_WEEK },
     });
   }
